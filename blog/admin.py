8617c4bbed7f20dd0a4166a7acec8a71d524e89a
kangfend <kangfend@gmail.com>
2014-09-27 09:55:32 +0700

    Buat models Category dan Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Post
+from django_markdown.admin import MarkdownModelAdmin
 
-# Register your models here.
+
+class PostAdmin(MarkdownModelAdmin):
+    exclude = ['author', 'slug']
+    list_display = ['title', 'author']
+    search_fields = ['title', 'author']
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+        obj.save()
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
--- portal/urls.py
+++ portal/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'portal.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = patterns('',
+    url(r'^markdown/', include('django_markdown.urls')),  # noqa
     url(r'^admin/', include(admin.site.urls)),
 )
