ffddd669752172d154db17172718e278b162453f
Fahri Reza <dozymoe@824484.no-reply.drupal.org>
2014-09-29 15:03:33 +0700

    asset dipindah ke bawah portal aja
    Tadinya mau coba asset sebagai app-nya sendiri, ternyata memang tidak
    bisa.

--- portal/settings.py
+++ portal/settings.py
@@ -46,9 +46,6 @@ INSTALLED_APPS = (
 
     #--Custom App--#
     'blog',
-
-    #--Global Assets--#
-    'asset',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -96,6 +93,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'portal', 'templates'),
+)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'portal', 'static'),
+)
 
 STATIC_URL = '/static/'
 
