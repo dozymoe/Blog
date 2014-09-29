64102640e2f1e5ab62512f12ca4f09c2f8b4a3bc
Fahri Reza <dozymoe@824484.no-reply.drupal.org>
2014-09-29 22:46:21 +0700

    fixture for blog app and create fixture command
    Kalau butuh semacam lorem ipsum waktu mengerjakan proyekan ini, coba
    jalankan jalankan `python manage.py blog_create_fixtures` untuk populasi
    data dari aplikasi blog.
    
    Data yang sama nantinya akan digunakan untuk menjalankan unittest.

---
+++ blog/fixtures.py
@@ -0,0 +1,301 @@
+"""fixtures for Blog app"""
+
+
+CATEGORIES = [{
+    'name': 'Anhelitus',
+    'is_active': False,
+}, {
+    'name': 'Spernitque',
+    'is_active': True,
+}, {
+    'name': 'Nominibus',
+    'is_active': True,
+}]
+
+
+POSTS = [{
+    'title': 'Stabis illa ambos',
+    'slug': 'stabis-illa-ambos',
+    'category': 'Nominibus',
+    'content': """
+## Intortos dixit veniam duarum
+
+Lorem markdownum quaeritur acuta, crinibus corpora profanus, cantibus. Coniunx
+est stamina, pro Claros Peneos ad quam: siderea Ityosque **ripis est** suo, pro.
+Pedum regale piscem hoc, sub herbas, paene anxia; fit.
+
+Vidit si liquores Haemonio, **aera** Pindo quodque? Est nec pervenit videntur
+fortia, fissa ab ad fecisses deo fuerat proturbat annoso. Mori fallis laterique
+pedes, grata conscendit per, a quercu *Pelasgi* latebra velit fuga, lammina,
+per. Corpus nam querella tempora ego fontibus virusque Minervae iunctae amori
+subiectatque ignara **ut se**.
+
+    boot_encoding.bitrate_task_output *=
+            software_northbridge_bluetooth.newbieBiometricsWins(
+            image_terabyte_java);
+    ipadScsiIcann.on(aspData + plagiarismUnixMpeg, xp(icfInternicDlc));
+    nonSouthbridgeBankruptcy(hdvMatrix, 4, 634218);
+    internetPrompt = insertionHeap;
+    duplexMatrix = pushRaster(cable_exif_target);
+
+Quidem fassusque plurimus ministro gravitas arcadiae *mirum* loquentem quisquis,
+enim momordi inquit locuturum, vigilans! Vidi innocuum dixit Iphis; ego tendere
+tu viae, pro arborea in subit minus? Saepe duas, ibi fiducia agentem Tantalus a
+auras sermone *Alcyone posita nubes* reponunt adesto, nostro videt: silvis.
+Terram meritorum et nisi, ignes, carpitur orabam, et cornua modo inque.
+
+## Latet pennis
+
+Liquidum ad et fore atque conubia, ipse gloria aequor effervescere sceptra
+minimum; ille, *e* quae pectore? Sim illic bello vobis vultus velantque servet
+membra verebor, est.
+
+1. Apertum aures exit nymphe vestrum
+2. Non domos decurrere in ille
+3. Relinquit teloque flebant vellet facibus
+4. Fatemur et Liternum dea habitavit ponunt tecta
+
+Videbitur felix **aqua**; pietate aura: *mox* est fidemque robora, in papilione
+ad. Intra et crimen ex occiderat illa ira volat qui, per. Cuspide saevior aera
+adsuetam, Macareus casus minimos Caeneus solebant, diffamatamque domo pressere.
+
+Icta nec suspiria longis requirenti eademque, in si hanc ore puella loquor. Cum
+fuga Hecateia steterant lugent incidis summam et cursus ille quotiens traxit
+placet licet; sublimis.""",
+
+    'publish': True,
+}, {
+    'title': 'Felix aut quod novum ferro ab Venerem',
+    'slug': 'felix-aut-quod-novum-ferro-ab-venerem',
+    'category': 'Nominibus',
+    'content': """
+## Nemorale concresse iacebat foramine posita
+
+Lorem markdownum pulcherrime simul arces volucris pervenit addit at quam nepotem
+turba. Cui tanti et excutiunt paretur, **aut noctem** inmensos; timidis mea, cum
+haut aratos; tacitos virgo. Ferarum superas et parato et laniatum, postera *et
+matris* silva, iungat! Precantia spumis Bacche peremptis Troiae denique, quo
+detegit quaerit tamen [sanguine](http://zombo.com/) Amphionis, sed deo.
+
+1. Victoria eadem nec memorat magnis concreta Quid
+2. Movensque voles invaserat Acmon collegit solvit
+3. Iuno qui ad
+
+Solis turbatus *vobis*. Movet iugalibus gelidae locus, est animo et pectus
+**intrare sospes** fundae dixerat pignora corpora consilium illius.
+
+## Aeneae quae
+
+Saepe questa ungues. Urbes amoris quae demptis tecta litore hoc, cur fugiens nec
+nec quid sit Procne praecontrectatque patebat miseroque. Omnes equam questaque
+noctes incessere modo sex res reges, mihi.
+
+1. Tectos videt undas metuunt paulatim iubar tepebat
+2. Vibrant isdem triplicis color
+3. Esset virgine
+4. Lycus ne laudant
+5. Et urbes et taedas
+
+Brevissima numeros Autolycus reducere pariter? Cum clivo fatalem molior revocare
+cupidine signum per deque per veluti nec nobis, mea. Terque tradidit et dolore
+*luctibus* substricta [Peneiaque utque Veneris](http://news.ycombinator.com/)
+sortemque iacto cingitur nitidum niveis anguibus et sanguis acta contemptrix.
+Harenas qua altera pro Cupido, ea uvae est, has illa effetum!
+
+Pylios Hectora ac atque, figuras noster. Meorum Palaemona stemus: erat penates
+aristas Nec una ingens nactus habenas lues regnum hinc: hora.
+
+[Peneiaque utque Veneris]: http://news.ycombinator.com/
+[sanguine]: http://zombo.com/""",
+
+    'publish': False,
+}, {
+    'title': 'Erat ausim',
+    'slug': 'erat-ausim',
+    'category': 'Nominibus',
+    'content': """
+## Cauti sua Tyria fera visus pingit illa
+
+Lorem markdownum Abarin Acasto prosunt coluisse movebere videre visis ego
+quibus, ipsa misit clausit truncum terrent. *Cum* ipsa vocabula, formaene,
+Tereus, est nostrum, in terra indigenae est iuncta. Circumspicit heros, et herba
+thalamos belloque, grata reddebat gratentur vana
+[totidem](http://heeeeeeeey.com/), exanimes [ad umbras](http://eelslap.com/) me.
+Quaeque emisit. Cyanee [cura](http://reddit.com/r/thathappened) regno, in
+quinque dextera.
+
+Poena in, cum onus quicquam supremum necis me tenuissimus et est ait: illo
+oppida bisque. Retinente tamen et locis venerande arboris aut, tota, nato!
+Subito dabat et et, ille ubi finge relinquis ab pariter saucia Tiberinaque
+inque, Phlegon. In terras ulcisci, non [adstricta
+Hactenus](http://www.wedrinkwater.com/) clivo aequora harundine.
+
+Dictaque atque edere pars. Tibi non litusque traditque tenebat carmen ignotos
+consorti videoque miserae summa frondibus dolos. Supremis *citharae* noceat;
+[nomen erat](http://heeeeeeeey.com/) in posset bracchia tibi cornua euntem.
+
+## Caeso Ocyroen Ligdo protinus adflat illi et
+
+Quem vidi ne faucibus novorum pervigilem Sirinosque cornua. Et pelagi. Foret
+posse iungere quin [capacibus similis innectite](http://www.mozilla.org/) ille:
+fuerat res. Virginibus ventura, et veneratur Mnemonidas atque.
+
+- Amantem qui
+- Ingemuit his et succidere capientur si Baccho
+- Altus misit agendo liquidum ut urbs summo
+
+## Ambo inter suorum
+
+Ille territa sit [irae ait](http://www.wedrinkwater.com/) conlegit mei axem
+anseribus tibi. Nominis aptus hanc iam vale: munere ad finis effugere foedusque
+duorum presserat tangit *explet*. Canes sed!
+
+    device = wi.leopardDiskReadme(lag_sound - sequence_dimm_ssid);
+    if (digitalPermalinkAccess(device_cold * 4, prebindingPage(5)) >=
+            text_system_offline(pplMemoryWorm, 2)) {
+        metalTftp += rpm_dv;
+        bar.cc_vlog(noc_odbc_meme + smishing, nicIo);
+    }
+    if (petaflopsArtAdd.del(mask, executableDrive) > rt_configuration_rate) {
+        adf_token_core.vleSdram = ddr_pci_command;
+    } else {
+        wep_uml_worm.access_cluster.in_supercomputer_software(mtu_metal_quad);
+        kilohertzModem += non_import;
+        vector.jre = scan_fi;
+    }
+    var bar_ntfs_ip = copyKoffice;
+    var defragmentSouthbridge = laserBufferToken(statusPodcast + -4,
+            internal_server.symbolic(944906, ocr_eup_xslt, 2)) - clock *
+            virusFontDomain;
+
+In acerbo possimne ad [rumor tu](http://www.wtfpl.net/) equi sic pater tum sed,
+et. Esse iunxit agros. Est nec negat, subiecta nebula, Philyreius te pars
+temptamina gentesque? Tale circueunt gladiis funes ignes Vulcanum **his Hylactor
+eloquioque** tumultus ignis.
+
+[ad umbras]: http://eelslap.com/
+[adstricta Hactenus]: http://www.wedrinkwater.com/
+[capacibus similis innectite]: http://www.mozilla.org/
+[cura]: http://reddit.com/r/thathappened
+[irae ait]: http://www.wedrinkwater.com/
+[nomen erat]: http://heeeeeeeey.com/
+[rumor tu]: http://www.wtfpl.net/
+[totidem]: http://heeeeeeeey.com/""",
+
+    'publish': True,
+}, {
+    'title': 'Euryte Amorum',
+    'slug': 'euryte-amorum',
+    'category': 'Spernitque',
+    'content': """
+## Vela idonea ipse dabat
+
+Lorem markdownum, et, *et luminis ultimus*, inque summum lenti mea nec, est.
+Enim plangi Saturnia autem. Hic in agnorunt, sanguine iubet. Caelique exitus;
+sis nec quoque, habentem *Persea auctor*.
+
+1. Vias quoque peremi casus
+2. Salire ut
+3. Et nec nigrumque
+4. Vellera nostrum rumpit
+5. Senectae hoc vero votivi Iove vola novat
+
+Lintea hortos. Vox Pallade lyram quaesita tuque. Mea gerit, Iuppiter secundo
+*praesens non* fugae est austri est quoque. Corpusque prosecta trepidare potuit
+et at pectoribus Talia nuda filius cadunt taurus discutiunt saevoque et sex
+gener. Dona cedit, ales **coniunx coeunt altissimus** senilem.
+
+## Magnis qua ignibus urbem nefas me illo
+
+Quoniam palude vituli, tendebat lacrimas Hersilien lectos pondere quandoquidem
+adest meique. Mare adiit fixa [est](http://www.youtube.com/watch?v=MghiBW3r65M);
+potens, **maesta**: haec praemia ossibus vulgusque infames sub saepe per. Duo
+vicisse colla fidibusque pavidum capiti pantherarum luna audito exigui inmemor.
+Circe vehi. Occuluere haurit: magis: *faciat aqua angues*, mihi modo urit: enim
+**suo** vocibus tanti.
+
+    smartphone_clone += rootkitDacWorkstation(dram(text_linkedin),
+            layout_progressive_app(webmaster(degauss_hfs, window,
+            bootXmlMultitasking), 30));
+    commercial_software.topology = 3 / 5 + 4;
+    urlApple(-3, -1, multithreadingIct);
+    if (enterpriseKilobit(nntp_duplex, word + bcc_latency,
+            kilohertzMemoryEps)) {
+        cifs(dfsBluetoothMbps);
+        sip_dma -= inkjet;
+        graphic_code_client.tooltip = latencyWired;
+    }
+
+In voces. Proiecta quos et **Cereale inmutat** criminis languida quoque nostras
+inmensos nondum. Stagna pectora Pylius ter venenis vivo, caruit, diu erant nec
+nostrae.
+
+Denique unda Iliacas tangeret auspiciis Leucothoeque aquas debere tegi quantum
+suos. Novi nonne obvia indicat arsuro est arsurusque mittentis sibilat damnandus
+tauros responsa, vos sim motis emicat. Nam paruerit Lyrnesia quis nostro
+rubescunt adflati tellus tellus contactu, viderat, orbem removente ipsorum
+superabitur ruricolae! Sedesque matutinaeque crede!
+
+[est]: http://www.youtube.com/watch?v=MghiBW3r65M""",
+
+    'publish': True,
+}, {
+    'title': 'Radios missae stupet fecit',
+    'slug': 'radios-missae-stupet-fecit',
+    'category': 'Nominibus',
+    'content': """
+## Modo adhuc sic cum cervicis
+
+Lorem markdownum quid anhelitus heros illa cupidine in deum *saxum* rogat
+*evanuit* numina multum, tinnitibus aeratas primos. Illo aut habent, sero
+titulum frustraque **illo** ieiunia spernitque, super et Aeolus Sole blandita
+inpensius elisi et. Et ignis cum membra, Mota vixisse, accipiunt genu tu
+nominibus infectaque sumit Argolica.
+
+1. Certa homini spectat
+2. Avus mihi promissa postquam metuendus palude
+3. Per deus opera diversi
+4. Siquis et viris
+5. Nulla quem Iunonis vultu inserui
+6. Plenissima nec tenus procubuisse
+
+Corpus solum dira recusem expetit *iaculo Haemoniis* vires, at quis fixit, iam.
+Distat *inguine* postquam in pacis.
+
+- Move gremio aetas
+- Cetera capillos
+- Mea est vos occidit mota labore spolia
+- Quae posset morientis cunctis
+- Sic quid
+- Genus Troiam facit repetitum ante et nisi
+
+## O Priamus quicquid dilataque in terrae inritamenta
+
+*Domos* portasque necopina tamen. Iuno has, non cani secant verbis, in gravidis
+**sidere** teque deerit respondit conantemque usque pectora fores.
+
+Ille Orion cunctae iussos, tulero ego parva cognoscet ponti. Geratis a quater
+manebit Oceanus quas. Qui equis levi: deam trunco iunctissima in humi mitius
+aera viro pallentem: aquas sed soror
+[conata](http://kimjongunlookingatthings.tumblr.com/)? Saucia es est scopulis ad
+haec suis Minervae correctis, comites, petiere promptum vulnera inertia
+aeternum. Et colorum cecropia, hunc icto *falleret* albentes bella quam mota
+danda?
+
+[Non](http://html9responsiveboilerstrapjs.com/) sine, dat cur natas fefellerat
+carpere quae cumulusque flebant mentem. Curvare dant eventus Narcissum bello
+vetus numquamque auratis; ipse dixit, haud vos tenentis.
+
+Iam cui recepto solutus exterrita inquit que enim deprensa tempora. Inquiri
+plures. *Has* est praecipue iamque genus, mora teque circumfunditur rogantem.
+Quae medios, quam [egens orbe](http://haskell.org/): credensque frater roganti
+crudus? Et [qui hostem avem](http://imgur.com/) Boreas Tyrrhenae incursu in
+**quoque virgaque dorso** ita et sacros?
+
+[Non]: http://html9responsiveboilerstrapjs.com/
+[conata]: http://kimjongunlookingatthings.tumblr.com/
+[egens orbe]: http://haskell.org/
+[qui hostem avem]: http://imgur.com/""",
+
+    'publish': True,
+}]
